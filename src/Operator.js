213603a283802b1667c2e29a1f1e0da231524e88
Phil at Xonox <36802525+CodeTrafficker@users.noreply.github.com>
2020-01-01 19:13:05 -0800

    Refactoring table, cleaning up mobile
    Refactored table into flexbox layout; fixed mobile layout issues; established mobile-first css with 600+ within media queries; removed CRA logos.

--- src/Operator.js
+++ src/Operator.js
@@ -3,35 +3,54 @@ import './operator.css';
 import MOCKCUSTOMERS from './mockData';
 
 class CustomerInfo extends React.Component {
+  constructor(props)  {
+    super(props);
+    this.state ={
+      value: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+
   render() {
     const mockCustomers = MOCKCUSTOMERS;
 
     const customerList = mockCustomers.map((customer)=>
-      <tr>
-        <td>{customer.name}</td>
-        <td>{customer.cc}</td>
-        <td>
+
+      // {this.state.value = customer.summary};
+      // {customer.summary}
+
+      <div className="customerData">
+        <div>{customer.name}</div>
+        <div>{customer.cc}</div>
+        <div>
           {customer.purchases.store1} - ${customer.purchases.amt1}<br />
           {customer.purchases.store2} - ${customer.purchases.amt2}<br />
           {customer.purchases.store3} - ${customer.purchases.amt3}<br />
-        </td>
-        <td>
-          <textarea value="{customer.summary}" />
-        </td>
-      </tr>
+        </div>
+        <form>
+          <textarea value="" onChange={this.handleChange}></textarea>
+          <button type="submit" onclick={this.handleSubmit}>Update summary</button>
+         </form>
+      </div>
     );
 
 
     return (
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>Credit Card #</th>
-          <th>Recent Purchases</th>
-          <th>Summary</th>
-        </tr>
+      <div className="opGrid">
+        <div className="opHeader">
+          <div>Name</div>
+          <div>Credit Card</div>
+          <div>Recent Purchases</div>
+        </div>
         {customerList}
-      </table>
+      </div>
     );
   }
 }
