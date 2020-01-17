7615199d93040858940a0556c3cc4ac42247bc6d
Phil at Xonox <36802525+CodeTrafficker@users.noreply.github.com>
2020-01-16 19:07:34 -0800

    Code clean-up: manifest updates
    Updated manifest to only two errors in inspector's application/Manifest auditor. ServiceWorker.js is in the manifest and index.js but err is not clearing. \_(`-`)_/
    
    Otherwise should be standard manifest.json.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 
 import Operator from './Operator';
@@ -31,5 +32,8 @@ export default function Routing() {
   );
 }
 
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
